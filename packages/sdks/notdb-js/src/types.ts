cdd8c182cd7384eb3e3dec60de414c76a56fa10e
titanium-dev <akinkunmioye42@gmail.com>
2025-06-20 23:21:50 +0100

    refactor(types): update field type definitions and introduce system fields for enhanced schema management

--- packages/sdks/notdb-js/src/types.ts
+++ packages/sdks/notdb-js/src/types.ts
@@ -1,9 +1,15 @@
-export type FieldType = "string" | "number" | "boolean";
+type SystemFields = {
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type JSONFieldType = "string" | "number" | "boolean";
 
 export type JSONSchema = {
   properties: {
     [key: string]: {
-      type: FieldType;
+      type: JSONFieldType;
       required?: boolean;
       unique?: boolean;
       default?: any;
@@ -15,24 +21,38 @@ export type SchemaDefinition = {
   [collection: string]: JSONSchema;
 };
 
-type InferFieldType<T extends FieldType> = T extends "string"
-  ? string
-  : T extends "number"
-    ? number
-    : T extends "boolean"
-      ? boolean
-      : never;
-
-export type InferSchemaProps<T extends JSONSchema["properties"]> = {
-  [K in keyof T as T[K]["required"] extends true ? K : never]: InferFieldType<
-    T[K]["type"]
-  >;
-} & {
-  [K in keyof T as T[K]["required"] extends true ? never : K]?: InferFieldType<
-    T[K]["type"]
-  >;
-};
-
 export interface CreateClientOptions {
   apiKey: string;
 }
+
+export type SelectFields<T> = {
+  [K in keyof T]?: true;
+};
+
+export type InferFieldType<T extends { type: JSONFieldType }> =
+  T["type"] extends "string"
+    ? string
+    : T["type"] extends "number"
+      ? number
+      : T["type"] extends "boolean"
+        ? boolean
+        : never;
+
+export type InferSchemaProps<T extends JSONSchema["properties"]> = {
+  [K in keyof T]: InferFieldType<T[K]>;
+} & SystemFields;
+
+export type InferSelected<T, S extends SelectFields<T> | undefined> =
+  S extends SelectFields<T>
+    ? {
+        [K in keyof S & keyof T]: S[K] extends true ? T[K] : never;
+      } & {
+        _id: string;
+        createdAt: string;
+        updatedAt: string;
+      }
+    : T & {
+        _id: string;
+        createdAt: string;
+        updatedAt: string;
+      };
