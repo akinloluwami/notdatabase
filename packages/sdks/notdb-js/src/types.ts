15dee67a6b0a9c4e49fd5c36cfea68ee21917572
titanium-dev <akinkunmioye42@gmail.com>
2025-06-21 00:06:08 +0100

    fix(types): make system fields optional for improved flexibility in type definitions

--- packages/sdks/notdb-js/src/types.ts
+++ packages/sdks/notdb-js/src/types.ts
@@ -1,7 +1,7 @@
 type SystemFields = {
-  _id: string;
-  createdAt: string;
-  updatedAt: string;
+  _id?: string;
+  createdAt?: string;
+  updatedAt?: string;
 };
 
 export type JSONFieldType = "string" | "number" | "boolean";
