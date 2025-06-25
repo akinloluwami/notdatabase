705681b5c52467eae5ed8b0dc5d86806f23fd886
titanium-dev <akinkunmioye42@gmail.com>
2025-06-25 18:18:52 +0100

    refactor(layout): restructure layout component for improved organization and styling

--- apps/web/app/docs/layout.tsx
+++ apps/web/app/docs/layout.tsx
@@ -8,33 +8,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="dark">
-      <body>
-        <div className="flex justify-between max-w-7xl mx-auto px-4 py-4 sticky top-0 backdrop-blur z-50">
-          <div className="flex items-center gap-1">
-            <Link href="/" className="flex items-center gap-2">
-              <img src="/logo.png" className="w-6" />
-              <p className="font-semibold">NotDatabase</p>
-            </Link>
-            <p className="text-gray-500">Docs</p>
-          </div>
-          <div className="text-sm text-gray-500 flex items-center gap-2">
-            <Link
-              href="https://github.com/akinloluwami/notdatabase"
-              className="p-1 hover:bg-white/10 rounded-sm transition-colors"
-              target="_blank"
-            >
-              <SiGithub size={20} />
-            </Link>
-          </div>
+    <div className="max-w-7xl mx-auto px-4 w-full">
+      <div className="flex justify-between mx-auto py-4 sticky top-0 backdrop-blur z-50">
+        <div className="flex items-center gap-1">
+          <Link href="/" className="flex items-center gap-2">
+            <img src="/logo.png" className="w-6" />
+            <p className="font-semibold">NotDatabase</p>
+          </Link>
+          <p className="text-gray-500">Docs</p>
         </div>
-        <div className="max-w-7xl mx-auto px-4 py-4 flex gap-x-20 h-screen overflow-y-auto mb-20">
-          <div className="fixed top-20 h-full w-64 hidden md:block">
-            <Sidebar />
-          </div>
-          <div className="w-[90%] ml-80 h-full">{children}</div>
+        <div className="text-sm text-gray-500 flex items-center gap-2">
+          <Link
+            href="https://github.com/akinloluwami/notdatabase"
+            className="p-1 hover:bg-white/10 rounded-sm transition-colors"
+            target="_blank"
+          >
+            <SiGithub size={20} />
+          </Link>
         </div>
-      </body>
-    </html>
+      </div>
+      <div className="flex gap-x-10">
+        <Sidebar />
+        {children}
+      </div>
+    </div>
   );
 }
